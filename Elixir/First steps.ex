a8f35d842c8d3856a5418dd52fd65e0dc238d3f9
Gumbers <gustavomarinsuppi@gmail.com>
2019-12-01 22:11:33 -0300

    feat: learn how control structures

--- Elixir/First steps.ex
+++ Elixir/First steps.ex
@@ -72,14 +72,129 @@ IO.puts "end each method"
 IO.puts ""
 IO.puts "each method"
 IO.inspect(Enum.map(list3, fn(x) -> inspect(x) end))
+
 IO.puts "end each method"
 IO.puts ""
 
 
+IO.puts Enum.min list
+IO.puts Enum.min [], fn -> 0 end
+
+IO.puts Enum.max list
+IO.puts Enum.max [], fn -> 0 end
+
+IO.puts inspect Enum.filter list3, fn(x) -> is_number(x) end
+
+integerList = [1,3,5,7,2,4,6,8]
+
+IO.puts Enum.reduce integerList, fn(x, acc) -> acc + x end
+
+IO.puts Enum.reduce integerList, 100, fn(x, acc) -> acc + x end
+
+IO.puts inspect Enum.sort integerList
+
+IO.puts inspect  Enum.sort integerList, fn(x, y) -> x > y end
+
+duplicatedIntegerList = integerList ++ integerList
+
+IO.puts inspect duplicatedIntegerList
+IO.puts inspect Enum.uniq duplicatedIntegerList
+
+IO.puts inspect Enum.uniq_by duplicatedIntegerList, fn(x) -> x end
+
+x = 5
+y = 3
+{x,y} = {y,x}
+IO.puts x
+IO.puts y
 
 
+hello = "hello"
+
+greet = fn
+    (^hello,name) -> "Hi my friend #{name}"
+    (hello,name) -> "#{hello} my friend #{name}"
+end
+
+IO.puts greet.("hello", "batman")
+
+IO.puts greet.("Mornin'", "batman")
+
+IO.puts greet.("hello", "batman")
+
+IO.puts ""
+IO.puts "___________control structures___________"
+
+if "string value" do
+    IO.puts "string value valid"
+else
+    IO.puts "invalid"
+end
+
+unless is_integer("string") do
+    IO.puts "Not an integer"
+end
+
+case {:ok, "Hello"} do
+    {:ok, result} -> IO.puts result
+    {:error} -> IO.puts "Uh oh!"
+    _ -> IO.puts"Catch all"
+end
+
+anotherHero = "batman"
+
+case "Iron man" do
+    ^anotherHero -> IO.puts "Batman is good"
+    hero -> IO.puts "#{hero} is good too"
+end
 
 
+case {1,2,3} do
+    {1,x,3} when x > 1 -> IO.puts "Yeahhh!!"
+    _ -> IO.puts "Not so good"
+end
+
+
+cond do
+    2 + 2 === 5 ->
+    IO.puts "Never"
+    
+    2 * 5 === 8 ->
+    IO.puts "Are you kidding me?"
+    
+    4 + 4 === 8 -> 
+    IO.puts "Ok, you are right"
+end
+
+cond do
+    1 + 1 === 3  -> IO.puts "Whatt??"
+    1 + 3 === 6 -> IO.puts "No, try again"
+    true -> IO.puts "Why are you using hack?"
+end
+
+maybeBatman = %{ first: "Bruce", last: "Wayne" }
+
+
+with {:ok, first} <- Map.fetch(maybeBatman, :first),
+{:ok, last} <- Map.fetch(maybeBatman, :last),
+do: IO.puts "Problably batman: " <> first <> " " <> last
+
+import Integer
+
+m = %{ a: 2, b: 3 }
+IO.puts inspect Map.fetch(m, :b)
+
+a = with {:ok, number} <- Map.fetch(m, :c),
+        true <- is_even(number) do
+        IO.puts "#{number} divided by 2 is #{div(number, 2)}"
+        :even
+    else
+        :error -> 
+            IO.puts "This item is not in the map"
+            :error
+        
+    _ -> IO.puts "It is odd number" 
+end
 
 
 
