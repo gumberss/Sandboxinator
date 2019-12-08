3637e5d8a5f5b939a7639886dca5f91e100d53c5
Gumbers <gustavomarinsuppi@gmail.com>
2019-12-08 20:51:53 -0300

    feat: Understand a little bit abount how to use IEx

---
+++ Elixir/Day 008/2 - IEx Helpers.ex
@@ -0,0 +1,13 @@
+
+#helps of iex:
+h Map
+h Map.merge/2
+
+#Information-> where it is, What it is, and so on:
+i Map
+
+#Recompile a single module:
+r MyProject
+
+#What types are availables in the module:
+t Map
