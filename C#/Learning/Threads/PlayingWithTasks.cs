1328efeba6fa12fea63e976a76287bf788b49a9f
Gumbers <gustavomarinsuppi@gmail.com>
2020-02-09 15:59:24 -0300

    chore: Join threads and present them

--- C#/Learning/Threads/PlayingWithTasks.cs
+++ C#/Learning/Threads/PlayingWithTasks.cs
@@ -9,9 +9,64 @@ namespace Learning.Threads
 
         public void Process()
         {
+            Thread.CurrentThread.Name = "Main Thread";
+            PresentThread(Thread.CurrentThread);
+
+            Thread t1 = new Thread(ExecutionFake);
+            t1.Name = "t1";
+            t1.Start();
+            t1.Join();// Join main thread with this thread, so main thread will wait for this thread
+
+            Thread t2 = new Thread(() => ExecutionFake());
+            t2.Name = "t2";
+            t2.Start();
+            t2.Join();
+            
+
+            ParameterizedThreadStart ps = new ParameterizedThreadStart(p => ExecutionFakeParameter(p));
+
+            Thread t3 = new Thread(ps);
+            t3.Name = "t3";
+            t3.Start("Parameter");
+            t3.Join();
+
+            Thread t4 = new Thread(() =>
+            {
+                PresentThread(Thread.CurrentThread);
+
+                while (true)
+                {
+                    Console.WriteLine("Tic");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Tac");
+                    Thread.Sleep(1000);
+                }
+            });
+            t4.Name = "t4";
+            t4.Start();
+
+            Console.WriteLine("press any key to stop the tic tac :D");
+            Console.ReadKey();
+            t4.Join();
+        }
+
+        private void PresentThread(Thread t)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Name: {t.Name}");
+            Console.WriteLine($"CurrentCulture: {t.CurrentCulture}");
+            Console.WriteLine($"Priority: {t.Priority}");
+            Console.WriteLine($"ExecutionContext: {t.ExecutionContext}");
+            Console.WriteLine($"IsBackground: {t.IsBackground}");
+            Console.WriteLine($"IsThreadPoolThread: {t.IsThreadPoolThread}");
+            Console.WriteLine();
+        }
+
+        public void ParameterizedThreadStart()
+        {
             Thread t1 = new Thread(ExecutionFake);
             Thread t2 = new Thread(() => ExecutionFake());
-        
+
             t1.Start();
             t2.Start();
 
@@ -23,6 +78,8 @@ namespace Learning.Threads
 
         private void ExecutionFake()
         {
+            PresentThread(Thread.CurrentThread);
+
             Console.WriteLine("Executing....");
             Thread.Sleep(1000);
             Console.WriteLine("Ok, that's it");
@@ -31,6 +88,8 @@ namespace Learning.Threads
 
         private void ExecutionFakeParameter(object o)
         {
+            PresentThread(Thread.CurrentThread);
+
             Console.WriteLine($"Executing {o}....");
 
             Thread.Sleep(1000);
