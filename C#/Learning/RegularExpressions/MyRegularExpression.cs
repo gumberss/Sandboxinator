ac929b4856eea9d02f6b7145bc5a949ee4bef782
Gumbers <gustavomarinsuppi@gmail.com>
2020-02-13 20:45:13 -0300

    chore: regular expression review

---
+++ C#/Learning/RegularExpressions/MyRegularExpression.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Learning.RegularExpressions
+{
+    public class MyRegularExpression
+    {
+        public void Process()
+        {
+            String text = "C#   Java    Ruby   Elixir    Javascript";
+
+
+            var replaced = Regex.Replace(text, " +", ",");
+
+            Console.WriteLine(replaced);
+
+            String text2 = "123: O exterminador do futuro:1984:107";
+
+            //var pattern = "\\d+:[\\w ]+:[0-9]{4}:[0-9]+";
+            //var pattern = "\\d+:[a-zA-Z]+:[0-9]{4}:[0-9]+";
+            var pattern = "^\\d+:[a-zA-Z ]+:[0-9]{4}:[0-9]+$";
+
+            var replaced2 = Regex.IsMatch(text2, pattern);
+
+            Console.WriteLine(replaced2);
+        }
+
+
+    }
+}
