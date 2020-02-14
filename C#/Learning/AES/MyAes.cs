a447b18cac7bc7a2cea1b7e2262193b999dc090d
Gumbers <gustavomarinsuppi@gmail.com>
2020-02-13 23:41:15 -0300

    chore: decoding a string with AES

--- C#/Learning/AES/MyAes.cs
+++ C#/Learning/AES/MyAes.cs
@@ -6,31 +6,59 @@ namespace Learning.AES
 {
     public class MyAes
     {
+        public void Process2()
+        {
+
+        }
+
         public void Process()
         {
             String secret = "I need to protect it";
+            String decodedText = String.Empty;
 
-            byte[] encrypted, key;
+            byte[] encrypted, key, initializationVector;
 
             using (Aes aes = Aes.Create())
             {
                 Console.WriteLine(secret);
 
                 key = aes.Key;
+                initializationVector = aes.IV;
 
                 ICryptoTransform cryptoTransform = aes.CreateEncryptor();
 
-                using MemoryStream ms = new MemoryStream();
-                using CryptoStream cryptoStream = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write);
-                using StreamWriter sw = new StreamWriter(cryptoStream);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+                    using (StreamWriter sw = new StreamWriter(cryptoStream))
+                    {
+                        sw.Write(secret);
 
-                sw.Write(secret);
-
-                encrypted = ms.ToArray();
+                    }
+                    encrypted = ms.ToArray();
+                }
             }
 
             ShowBytes("Key: ", key);
             ShowBytes("Encrypted: ", encrypted);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = initializationVector;
+
+                ICryptoTransform decoder = aes.CreateDecryptor();
+
+                using (MemoryStream memoryStream = new MemoryStream(encrypted))
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decoder, CryptoStreamMode.Read))
+                using (StreamReader streamReader = new StreamReader(cryptoStream))
+                {
+                    decodedText = streamReader.ReadToEnd();
+                }
+            }
+
+            Console.WriteLine($"Decoded: {decodedText}");
+
         }
 
         public void ShowBytes(String title, byte[] bytes)
