e0487f499e73c2f9fa69f3e0fa0ff3dd9206548b
Gumbers <gustavomarinsuppi@gmail.com>
2020-02-15 09:58:15 -0300

    enjoying async atask again, it is so fun!

---
+++ C#/Learning/Threads/PlayingAsyncTask.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Learning.Threads
+{
+    public class PlayingAsyncTask
+    {
+        public void Process()
+        {
+            Stopwatch s = new Stopwatch();
+
+            s.Start();
+
+            RefatorWithTask();
+
+            s.Stop();
+
+            Console.WriteLine($"Time: {s.ElapsedMilliseconds}");
+        }
+
+        /// <summary>
+        /// Time: 358
+        /// </summary>
+        private void RefatorWithTask()
+        {
+            var tasks = new List<Task>(200);
+
+            foreach (var age in Enumerable.Range(1, 200))
+            {
+                tasks.Add(UsingTask(age));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private async Task UsingTask(int age)
+        {
+            Console.WriteLine($"Hello, I'm {age} old");
+
+            await Task.Delay(50);
+        }
+
+        /// <summary>
+        /// Time: 10439
+        /// </summary>
+        private static void Original()
+        {
+            foreach (var age in Enumerable.Range(1, 200))
+            {
+                Console.WriteLine($"Hello, I'm {age} old");
+
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
