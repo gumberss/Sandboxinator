aae76be921e81e312d08e1a655f33b640b7b77b0
Gumbers <gustavomarinsuppi@gmail.com>
2020-02-16 12:17:41 -0300

    chore: Getting hash with SHA256 algorithm

---
+++ C#/Learning/Hash/MyHash.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Learning.Hash
+{
+    public class MyHash
+    {
+        public void Process()
+        {
+            UTF8Encoding encoding = new UTF8Encoding();
+
+            var messageBytes = encoding.GetBytes("My message");
+
+            var algorithm = SHA256.Create();
+
+            var hash = algorithm.ComputeHash(messageBytes);
+
+            hash.ToList().ForEach(x => Console.Write($"{x} "));
+        }
+    }
+}
