757f459d93ed312c18389d77fe6676ec0b4845be
Gumbers <gustavomarinsuppi@gmail.com>
2020-02-21 20:32:23 -0300

    chore: ussing attributes to make condictions in the code

---
+++ C#/Learning/Reflection/MyReflection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learning.Reflection
+{
+    public class MyReflection
+    {
+        public void Process()
+        {
+            Console.WriteLine(Serialize(new MyObj()));
+            Console.WriteLine(Serialize(new MySerializableObj()));
+        }
+
+        public String Serialize<T>(T obj)
+        {
+            if (Attribute.IsDefined(typeof(T), typeof(SerializableAttribute)))
+            {
+                return "Ok! I did it :D";
+            }
+
+            return "Oh no!! I can't do this!";
+
+        }
+
+    }
+
+    public class MyObj
+    {
+
+    }
+
+    [Serializable]
+    public class MySerializableObj
+    {
+
+    }
+}
