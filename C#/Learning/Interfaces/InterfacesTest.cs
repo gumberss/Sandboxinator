09983cc7c094e8e9407d93f6b14b86e88e491e24
Gumbers <gustavomarinsuppi@gmail.com>
2020-02-22 13:25:50 -0300

    chore: interesting how you can make two behaviors for two interfaces in the same object.

---
+++ C#/Learning/Interfaces/InterfacesTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Learning.Interfaces
+{
+    public class InterfacesTest
+    {
+        public void Process()
+        {
+            var batCave = new BatCave();
+            batCave.Start();
+            (batCave as IHome).Start();
+            (batCave as IOffice).Start();
+
+            Console.WriteLine();
+
+            var privateBatCave = new SpecificBehaviorBatCave();
+
+            (privateBatCave as IHome).Start();
+            (privateBatCave as IOffice).Start();
+        }
+    }
+
+    public class BatCave : IHome, IOffice
+    {
+        //public method, can be only one
+        public void Start()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+
+    public interface IHome { void Start(); }
+
+    public interface IOffice { void Start(); }
+
+    public class SpecificBehaviorBatCave : IHome, IOffice
+    {
+        //When you have two interfaces with different behaviors,
+        //you must have a method without modifier and you specify the interface name 
+        void IHome.Start()
+        {
+            Console.WriteLine("Home start");
+        }
+
+        void IOffice.Start()
+        {
+            Console.WriteLine("Office start");
+        }
+    }
+}
