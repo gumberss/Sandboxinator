b499a095f541e2cfaee9ea9f49274c4d45cea47c
Gumbers <gustavomarinsuppi@gmail.com>
2020-02-25 22:57:09 -0300

    chore: writing, reading and showing data from file

--- C#/Learning/IO/MyFileStream.cs
+++ C#/Learning/IO/MyFileStream.cs
@@ -1,10 +1,49 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Learning.IO
 {
     public class MyFileStream
     {
         public void Process()
+        {
+
+            using (FileStream stream = new FileStream("OutputFile.txt", FileMode.Create, FileAccess.Write))
+            {
+                string message = "My beautiful message.";
+
+                byte[] array = Encoding.UTF8.GetBytes(message);
+                int position = 0;
+                int length = message.Length;
+
+                stream.Write(array, position, length);
+            }
+
+            using (FileStream stream = new FileStream("OutputFile.txt", FileMode.Append, FileAccess.Write))
+            {
+                string message = " My second part of my beautiful message";
+
+                byte[] array = Encoding.UTF8.GetBytes(message);
+                int position = 0;
+                int length = message.Length;
+
+                stream.Write(array, position, length);
+            }
+
+            using (FileStream stream = new FileStream("OutputFile.txt", FileMode.Open, FileAccess.Read))
+            {
+                byte[] array = new byte[stream.Length];
+                int position = 0;
+
+                stream.Read(array, position, array.Length);
+
+                Console.WriteLine(Encoding.UTF8.GetString(array));
+            }
+
+        }
+
+        private static void ReadWithFileStream()
         {
             int bytesLength = 10;
             byte[] data = new byte[bytesLength];
@@ -31,7 +70,7 @@ namespace Learning.IO
                 System.Console.WriteLine();
 
                 stream.Seek(5, SeekOrigin.Current);
-                
+
                 stream.Read(data, position, bytesLength);
 
                 foreach (var item in data)
@@ -61,8 +100,6 @@ namespace Learning.IO
                     System.Console.Write((char)item);
                 }
             }
-
-
         }
     }
 }
