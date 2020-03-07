b7eed664a5ca0315a38b72165d025415f4bc3ee0
Gumbers <gustavomarinsuppi@gmail.com>
2020-03-07 11:24:52 -0300

    chore: Using trace

---
+++ C#/Learning/Traces/MyTrace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Learning.Traces
+{
+    public class MyTrace
+    {
+        public void Process()
+        {
+            Trace.WriteLine("Test1");
+            Trace.WriteLine("Test2");
+            Trace.WriteLine("Test3");
+            Trace.WriteLine("Test4");
+            Trace.WriteLine("Test5");
+        }
+    }
+}
