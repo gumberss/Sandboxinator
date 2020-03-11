f5f097f6af2a49f010d7cecce50c21433c68f326
Gumbers <gustavomarinsuppi@gmail.com>
2020-03-11 00:17:54 -0300

    refactor: using pattern matching to make my code a little bit mor beautiful

--- Elixir/ElixirBootcamp/cards/lib/cards.ex
+++ Elixir/ElixirBootcamp/cards/lib/cards.ex
@@ -47,11 +47,11 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, binary} = File.read(filename)
 
-    case status do
-      :ok -> :erlang.binary_to_term binary
-      :error -> "Error"
+
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _} -> "Error"
     end
   end
 
