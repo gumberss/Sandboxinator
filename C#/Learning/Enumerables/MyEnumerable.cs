a8f54e5abba80ae35111a76a7f2804213afddea8
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-04 20:29:50 -0300

    chore: Learning enumerable

---
+++ C#/Learning/Enumerables/MyEnumerable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learning.Enumerables
+{
+    public class MyEnumerable
+    {
+        public void Process()
+        {
+
+        }
+
+    }
+
+    public class MyFunnyEnumerable : IEnumerable<int>
+    {
+        private List<int> _list;
+
+        public MyFunnyEnumerable()
+        {
+            _list = new List<int>();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+    }
+}
