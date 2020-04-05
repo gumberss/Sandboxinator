a20014fbdbfa98bbc66ac3705c87e68e18f4d37b
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-04 21:23:27 -0300

    chore: Learning how attributes work with private and public properties

---
+++ C#/Learning/Attributes/MyAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learning.Attributes
+{
+    //author is a positional parameter because it is in the constructor 
+    //and modified is a named parameter because it is a public property that is not in the constructor
+    //http://www.java2s.com/Tutorial/CSharp/0200__Attribute/PositionalvsNamedParameters.htm
+    [MyAtt(author: "hi", Modified = "hi")]
+    public class MyAttribute
+    {
+
+        public void Process()
+        {
+
+        }
+    }
+
+    public class MyAtt : Attribute
+    {
+        private String Author;
+        public string Modified;
+
+        public MyAtt(String author)
+        {
+            Author = author;
+            Modified = DateTime.Now.ToString();
+        }
+    }
+}
