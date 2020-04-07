f089b1e1a2739a2a07214b755d5137387fb310bc
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-06 22:09:27 -0300

    chore: Looking how EventLog works

---
+++ C#/LearningFramework/EventLogs/MyEventLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace LearningFramework.EventLogs
+{
+    public class MyEventLog
+    {
+        public void Process()
+        {
+            if (!EventLog.SourceExists("MySource"))
+            {
+                EventLog.CreateEventSource("MySource", "AppCenter");
+            }
+
+            EventLog.WriteEntry("MySource", "A static Error", EventLogEntryType.Error);
+            EventLog.WriteEntry("MySource", "A static Information", EventLogEntryType.Information);
+            EventLog.WriteEntry("MySource", "A static Warning", EventLogEntryType.Warning);
+
+            EventLog myLogger = new EventLog("AppCenter");
+            
+            myLogger.Source = "MySource";
+
+            myLogger.WriteEntry("An Error", EventLogEntryType.Error);
+            myLogger.WriteEntry("A Information", EventLogEntryType.Information);
+            myLogger.WriteEntry("A Warning", EventLogEntryType.Warning);
+        }
+    }
+}
