1d1f4f9ae32805b71129a33073fe009223666f67
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-06 23:50:20 -0300

    chore: Adding Listener to NetworkStream

--- C#/Learning/ReaderAndWriter/MyNetworkStream.cs
+++ C#/Learning/ReaderAndWriter/MyNetworkStream.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Learning.ReaderAndWriter
 {
@@ -10,7 +12,11 @@ namespace Learning.ReaderAndWriter
     {
         public void Process()
         {
-            using (var client = new TcpClient("localhost", 80))
+            new Thread(Listen).Start();
+
+            Thread.Sleep(1000);
+
+            using (var client = new TcpClient("localhost", 500))
             using (NetworkStream ns = client.GetStream())
             {
                 StreamWriter writer = new StreamWriter(ns);
@@ -18,6 +24,22 @@ namespace Learning.ReaderAndWriter
                 writer.Write("Hey!");
 
                 writer.Flush();
+            }
+        }
+
+        public void Listen()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 500);
+            listener.Start();
+
+            TcpClient receiverClient = listener.AcceptTcpClient();
+
+            NetworkStream stream = receiverClient.GetStream();
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                String message = reader.ReadToEnd();
+
+                Console.WriteLine(message);
             }
         }
     }
