13e3a1d9a36d4b53201a459a24ecbe6e52ebf93a
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-06 22:09:14 -0300

    chore: Looking disposable again

---
+++ C#/Learning/ObjectLifeCycle/MyOtherDisposable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learning.ObjectLifeCycle
+{
+    public class MyOtherDisposable
+    {
+        public void Process()
+        {
+
+            var dispo = new Dispo();
+
+            
+
+        }
+    }
+
+
+    public class Dispo : IDisposable
+    {
+        void IDisposable.Dispose()
+        {
+            //dispose
+        }
+    }
+}
