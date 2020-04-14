c2205511da4737b645fd060b6ffd91356e309d1d
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-13 22:52:50 -0300

    chore: adding length test of create_deck

--- Elixir/ElixirBootcamp/cards/test/cards_test.exs
+++ Elixir/ElixirBootcamp/cards/test/cards_test.exs
@@ -2,4 +2,9 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  test "ceate_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+
+    assert deck_length == 20
+  end
 end
