296ed8a0da461b7268647eb52e1ad900c986cc12
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-13 22:58:32 -0300

    chore: adding test to shuffle method using refute to assert

--- Elixir/ElixirBootcamp/cards/test/cards_test.exs
+++ Elixir/ElixirBootcamp/cards/test/cards_test.exs
@@ -7,4 +7,10 @@ defmodule CardsTest do
 
     assert deck_length == 20
   end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+
+    refute deck == Cards.shuffle(deck)
+  end
 end
