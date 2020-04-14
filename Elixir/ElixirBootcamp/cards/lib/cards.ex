9577114907678fb714ab298a7f7e5cf240390dcd
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-13 21:58:49 -0300

    feat: adding documentation for deal function

--- Elixir/ElixirBootcamp/cards/lib/cards.ex
+++ Elixir/ElixirBootcamp/cards/lib/cards.ex
@@ -1,17 +1,11 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+    Provides methods for creating and handling a deck of cards
+    run mix docs to create a documentation
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-      Comand: iex -S mix
+    Returns a list of strings representing a deck of playing cards
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "five"]
@@ -37,6 +31,18 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
