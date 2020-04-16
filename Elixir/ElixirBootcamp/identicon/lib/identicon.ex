ad4fa5fd8093baa70ebd4cf27a483f41a1f180be
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-15 23:13:50 -0300

    feat: finishing method color_pick using alot of pattern matching \o/

--- Elixir/ElixirBootcamp/identicon/lib/identicon.ex
+++ Elixir/ElixirBootcamp/identicon/lib/identicon.ex
@@ -6,16 +6,14 @@ defmodule Identicon do
   end
 
   def hash_input(input) do
-
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(image) do
-    %Identicon.Image{ hex: [r, g, b | _tail] } = image
-
-    [r, g, b]
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 end
