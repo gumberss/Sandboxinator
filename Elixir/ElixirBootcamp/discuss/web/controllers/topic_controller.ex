9362f5916eebf2f0b0b9a11c1c5c47c14b868c01
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-24 21:55:47 -0300

    chore: adding topic view

--- Elixir/ElixirBootcamp/discuss/web/controllers/topic_controller.ex
+++ Elixir/ElixirBootcamp/discuss/web/controllers/topic_controller.ex
@@ -1,7 +1,10 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
   def new(conn, params) do
+
+    changeset = Topic.changeset( %Topic{}, %{})
 
 
   end
