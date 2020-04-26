257473eaad1064a707426cc48818e7121aeae054
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-26 14:34:53 -0300

    chore: adding content to topic create method

--- Elixir/ElixirBootcamp/discuss/web/controllers/topic_controller.ex
+++ Elixir/ElixirBootcamp/discuss/web/controllers/topic_controller.ex
@@ -12,6 +12,14 @@ defmodule Discuss.TopicController do
 
   def create(conn, %{"topic" => topic}) do
 
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      { :ok, post } -> IO.inspect post
+      { :error, changeset } -> IO.inspect changeset
+    end
+
+    conn
   end
 
 end
