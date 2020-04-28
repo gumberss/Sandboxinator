27db7a3ca24d2a87e32f2d75db00bef4fb6471b0
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-27 21:57:16 -0300

    chore: adding screen with a list of topics

--- Elixir/ElixirBootcamp/discuss/web/controllers/topic_controller.ex
+++ Elixir/ElixirBootcamp/discuss/web/controllers/topic_controller.ex
@@ -3,6 +3,14 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
+  def index(conn, _params) do
+
+    topics = Repo.all(Topic)
+
+
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
 
     changeset = Topic.changeset( %Topic{}, %{})
@@ -19,7 +27,6 @@ defmodule Discuss.TopicController do
       { :error, changeset } ->
         render conn, "new.html", changeset: changeset
     end
-
   end
 
 end
