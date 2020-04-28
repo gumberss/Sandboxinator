32101922348a29925d7aafc2f671696c220b4373
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-28 20:52:33 -0300

    chore: adding edit route and edit function in topic controller

--- Elixir/ElixirBootcamp/discuss/web/controllers/topic_controller.ex
+++ Elixir/ElixirBootcamp/discuss/web/controllers/topic_controller.ex
@@ -23,13 +23,17 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      { :ok, post } ->
+      { :ok, _post } ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: topic_path(conn, :index))
       { :error, changeset } ->
         render conn, "new.html", changeset: changeset
     end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+
   end
 
 end
