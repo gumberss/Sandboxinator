3ec0d5c114bdafacbb32c4492c3081d6ba983706
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-28 23:38:16 -0300

    chore: solve a swap problem with elixir

---
+++ Elixir/SolvedProblems/swap.ex
@@ -0,0 +1,30 @@
+#https://www.hackerrank.com/challenges/string-o-permute/problem
+defmodule Solution do
+
+    def solve([]), do: []
+    def solve([current| tail]) do
+        swap_array current
+        solve tail
+    end
+
+    def swap_array(data) do
+        data
+        |> String.to_charlist
+        |> swap
+        |> List.to_string
+        |> IO.puts
+    end
+
+    
+    def swap([]), do: []
+    def swap([first, second | tail]) do
+        [second, first] ++ swap(tail)
+    end
+    
+end
+
+
+[_ | rest] = IO.read(:all) 
+    |> String.split("\n")
+
+Solution.solve(rest)
