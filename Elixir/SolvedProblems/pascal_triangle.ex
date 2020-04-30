908bd38e73fdeee898e411dc7f237693fa7e8239
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-30 00:11:57 -0300

    refac: Another way to solve Pascal's triangle with Elixir

--- Elixir/SolvedProblems/pascal_triangle.ex
+++ Elixir/SolvedProblems/pascal_triangle.ex
@@ -25,3 +25,40 @@ end
  lines = IO.read(:all)
 {value, _} =  Integer.parse(lines)
 Solution.solve(value, [])
+
+
+
+####################
+
+#Another way
+####################
+
+
+defmodule Solution do
+
+    def solve(1, _), do: []
+    def solve(count, []) do
+        IO.puts 1
+        solve(count, [1])
+    end
+    def solve(count, old_line) do
+
+        new_line = sum_line(old_line)
+        
+        IO.puts Enum.join( new_line," ")
+        solve(count - 1, new_line)
+    end
+    
+    def sum_line(list), do: sum_line( [0] ++ list, Enum.reverse(list ) ++ [0])
+    def sum_line([ last ], [ last_second_list]), do: [last + last_second_list]
+    def sum_line([ first | first_tail], [first_second_list | second_tail]) do
+        [first + first_second_list] ++ sum_line(first_tail, second_tail)
+        
+    end
+
+end
+
+ lines = IO.read(:all)
+{value, _} =  Integer.parse(lines)
+Solution.solve(value, [])
+
