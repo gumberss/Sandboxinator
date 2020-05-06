c53b152611991c07b8dc8a97ae93f8ca8c0e2a48
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-05 21:13:19 -0300

    chore: twostring to one with elixir

---
+++ Elixir/SolvedProblems/two_to_one.ex
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/elixir
+
+defmodule TwoToOne do
+  def longest(a, b) do
+    String.to_charlist(a) ++ String.to_charlist(b)
+      |> MapSet.new
+      |> MapSet.to_list
+      |>List.to_string
+  end
+end
