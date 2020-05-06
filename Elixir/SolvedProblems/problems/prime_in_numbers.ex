5e3338f5f86e7441d26d66b19353ea500c438f00
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-06 17:43:47 -0300

    chore: prime in numbers with limit to calculate

--- Elixir/SolvedProblems/problems/prime_in_numbers.ex
+++ Elixir/SolvedProblems/problems/prime_in_numbers.ex
@@ -17,3 +17,26 @@ defmodule PrimesInNumbers do
   def factor({key, values}) when length(values) == 1, do: "(#{key})"
   def factor({key, values}), do: "(#{key}**#{length(values)})"
 end
+
+
+# My better way (adding limmit to calculate):
+
+defmodule PrimesInNumbers do
+  def prime_factors(n) do
+    n
+    |> mmc
+    |> Enum.group_by(&(&1))
+    |> Enum.map(&factor/1)
+    |> Enum.join
+  end
+
+  def mmc(n), do: mmc(n, 2, [], :math.sqrt(n))
+  def mmc(1, _div, divisors, _limit), do: divisors
+  def mmc(n, div, divisors, limit) when div == n, do: 1 |> mmc(n, [n] ++ divisors, limit)
+  def mmc(n, div, divisors, limit) when div > limit, do: n |> mmc(n, divisors, limit)
+  def mmc(n, div, divisors, limit) when rem(n, div) != 0, do: n |> mmc(div + 1, divisors, limit)
+  def mmc(n, div, divisors, limit), do: n / div |> round |> mmc(div, divisors ++ [div], limit)
+
+  def factor({key, values}) when length(values) == 1, do: "(#{key})"
+  def factor({key, values}), do: "(#{key}**#{length(values)})"
+end
