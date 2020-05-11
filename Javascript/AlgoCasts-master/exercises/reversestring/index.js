12951c56a03f39a52ad947782f800cd8f3c74d14
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-11 19:09:55 -0300

    chore: adding string reversal woth javascript

--- Javascript/AlgoCasts-master/exercises/reversestring/index.js
+++ Javascript/AlgoCasts-master/exercises/reversestring/index.js
@@ -2,10 +2,37 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((acc, c) =>  c + acc)
+}
+
+
+
+/**
+ * var newString = "";
+
+    for(var i = 0; i< str.length;i++){
+        newString = str[i] + newString
+    }
+    
+    return newString
+ */
+
+/*
+function reverse(str) {
+    return reverseStr(str, "", str.length-1)
+}
+
+function reverseStr(str, newString, count) {
+
+    if (count == -1) return newString
+
+    return reverseStr(str,  newString + str[count], --count)
+}
+*/
 
 module.exports = reverse;
