dc48152ce2884e7448b462a57b811c1287f2ab0e
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-12 20:33:33 -0300

    chore: adding migration to add user reference in topics table

--- Elixir/ElixirBootcamp/discuss/web/controllers/topic_controller.ex
+++ Elixir/ElixirBootcamp/discuss/web/controllers/topic_controller.ex
@@ -2,7 +2,7 @@ defmodule Discuss.TopicController do
 
   use Discuss.Web, :controller
   alias Discuss.Topic
-  #plug Discuss.Plugs.RequireAuth when action in [ :new, :create, ]
+  plug Discuss.Plugs.RequireAuth when action in [ :new, :create, :edit, :update, :delete]
 
 
   def index(conn, _params) do
