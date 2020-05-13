31a5a7f5f63db2ca7fa2639bfe47157575237ae1
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-13 17:13:18 -0300

    chore: recive a comment in comment channel when user add comment on screen

--- Elixir/ElixirBootcamp/discuss/web/channels/comments_channel.ex
+++ Elixir/ElixirBootcamp/discuss/web/channels/comments_channel.ex
@@ -1,15 +1,30 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  def join(name, _params, socket) do
-    IO.puts  "++++++++"
-    IO.puts name
-    IO.puts  "++++++++"
+  alias Discuss.{Topic, Comment}
 
-    {:ok, %{hey: "there"}, socket }
+  def join("comments:" <> topic_id, _params, socket) do
+
+    topic_id = String.to_integer(topic_id)
+
+    topic = Repo.get(Topic, topic_id)
+
+
+    {:ok, %{hey: "there"}, assign(socket, :topic, topic) }
   end
 
-  def handle_in() do
+  def handle_in(_name, %{"content" => content}, socket) do
 
+    topic = socket.assigns.topic
+
+    changeset = topic
+    |> build_assoc(:comments)
+    |> Comment.changeset(%{ content: content })
+
+    case Repo.insert(changeset) do
+      {:ok, _comment} -> {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
--- Elixir/ElixirBootcamp/discuss/web/static/js/socket.js
+++ Elixir/ElixirBootcamp/discuss/web/static/js/socket.js
@@ -4,15 +4,22 @@ let socket = new Socket("/socket", { params: { token: window.userToken } });
 
 socket.connect();
 
-let channel = socket.channel("comments:1", {});
+const createSocket = (topicId) => {
+  let channel = socket.channel(`comments:${topicId}`, {});
 
-channel
-  .join()
-  .receive("ok", (resp) => {
-    console.log("Joined successfully", resp);
-  })
-  .receive("error", (resp) => {
-    console.log("Unable to join", resp);
+  channel
+    .join()
+    .receive("ok", (resp) => {
+      console.log("Joined successfully", resp);
+    })
+    .receive("error", (resp) => {
+      console.log("Unable to join", resp);
+    });
+
+  document.querySelector("button").addEventListener("click", () => {
+    const content = document.querySelector("textarea").value;
+    channel.push("comment:add", { content: content });
   });
+};
 
-export default socket;
+window.createSocket = createSocket;
