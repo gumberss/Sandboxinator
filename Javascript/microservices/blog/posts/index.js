d4c5291f126de46b4b663c6eb159f84b21f1acdd
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-14 15:41:28 -0300

    chore: adding start files of microservice with nodejs and react

---
+++ Javascript/microservices/blog/posts/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json())
+
+const posts = {};
+
+app.get("/posts", (req, resp) => {
+  resp.send(posts);
+});
+
+app.post("/posts", (req, resp) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.boy;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  resp.status(201).send(post[id])
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
