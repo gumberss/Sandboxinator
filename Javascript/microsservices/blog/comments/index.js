5db61f0a84bd3c0776a7d4e320aceb97e195ab8a
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-15 20:41:22 -0300

    chore: now, comment microsservice understand events from moderation microsservice

--- Javascript/microsservices/blog/comments/index.js
+++ Javascript/microsservices/blog/comments/index.js
@@ -36,15 +36,37 @@ app.post('/posts/:id/comments', async (req, resp) => {
 			id: commentId,
 			content,
 			postId: req.params.id,
-			status: 'pending'
+			status: 'pending',
 		},
 	})
 
 	resp.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
 	console.log('Received Event:', req.body.type)
+
+	const { type, data } = req.body
+
+	if (type === 'CommentModerated') {
+		const { postId, id, status, content } = data
+
+		const comments = commentsByPostId[postId]
+
+		const comment = comments.find(comment => comment.id === id)
+
+		comment.status = status
+
+		await axios.post('http://localhost:4005/events', {
+			type: '',
+			data: {
+				id,
+				postId,
+				status,
+				content
+			}
+		})
+	}
 
 	res.send({})
 })
