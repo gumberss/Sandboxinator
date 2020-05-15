c81feb2f66b416ecb7f67dee109590ea145cafaf
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-15 18:11:57 -0300

    chore: send an event when a post is created

--- Javascript/microsservices/blog/posts/index.js
+++ Javascript/microsservices/blog/posts/index.js
@@ -1,30 +1,39 @@
-const express = require("express");
+const express = require('express')
 const bodyParser = require('body-parser')
-const { randomBytes } = require("crypto");
+const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
-const app = express();
+const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-const posts = {};
+const posts = {}
 
-app.get("/posts", (req, resp) => {
-  resp.send(posts);
-});
+app.get('/posts', (req, resp) => {
+	resp.send(posts)
+})
 
-app.post("/posts", (req, resp) => {
-  const id = randomBytes(4).toString("hex");
-  const { title } = req.body;
+app.post('/posts', async (req, resp) => {
+	const id = randomBytes(4).toString('hex')
+	const { title } = req.body
 
-  posts[id] = {
-    id,
-    title,
-  };
+	posts[id] = {
+		id,
+		title,
+	}
 
-  resp.status(201).send(posts[id])
-});
+	await axios.post('http://localhost:4005/events', {
+		type: 'PostCreated',
+		data: {
+			id,
+			title,
+		},
+	})
+
+	resp.status(201).send(posts[id])
+})
 
 app.listen(4000, () => {
-  console.log("Listening on 4000");
-});
+	console.log('Listening on 4000')
+})
