97ec505f6a19e1b0c4f4dae8129b83e6a7be40fb
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-15 21:32:20 -0300

    chore: adding event bus storage and query microsservice retrieve these events

--- Javascript/microsservices/blog/event-bus/index.js
+++ Javascript/microsservices/blog/event-bus/index.js
@@ -3,10 +3,14 @@ const bodyParser = require('body-parser')
 const axios = require('axios')
 
 const app = express()
-
 app.use(bodyParser.json())
+
+const events = []
+
 app.post('/events', (req, res) => {
 	const event = req.body
+
+	events.push(event)
 
 	axios.post('http://localhost:4000/events', event)
 	axios.post('http://localhost:4001/events', event)
@@ -16,6 +20,10 @@ app.post('/events', (req, res) => {
 	res.send({ status: 'OK' })
 })
 
+app.get('/events', (req, res) => {
+	res.send(events)
+})
+
 app.listen(4005, () => {
-    console.log('Listining on 4005')
+	console.log('Listining on 4005')
 })
--- Javascript/microsservices/blog/query/index.js
+++ Javascript/microsservices/blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -12,9 +13,7 @@ app.get('/posts', (req, res) => {
 	res.send(posts)
 })
 
-app.post('/events', (req, res) => {
-	const { type, data } = req.body
-
+const handleEvent = (type, data) => {
 	if (type === 'PostCreated') {
 		const { id, title, status } = data
 		posts[id] = {
@@ -30,8 +29,8 @@ app.post('/events', (req, res) => {
 
 		post.comments.push({
 			id,
-            content,
-            status
+			content,
+			status,
 		})
 	} else if (type === 'CommentUpdated') {
 		const { id, content, status, postId } = data
@@ -43,10 +42,23 @@ app.post('/events', (req, res) => {
 		comment.status = status
 		comment.content = content
 	}
+}
+
+app.post('/events', (req, res) => {
+	const { type, data } = req.body
+
+	handleEvents(type, data)
 
 	res.send({})
 })
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
 	console.log('Listening on 4002')
+
+	const res = await axios.get('http://localhost:4005/events')
+
+	for (let event of res.data) {
+        console.log('Processing event: ', event.type)
+        handleEvent(event.type, event.data)
+	}
 })
