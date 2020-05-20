d43d498311d8e7c3e5129d56ef3e082b85cc8b3c
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-20 19:05:44 -0300

    feat: loking how net core and ner framework 452 work with redis

---
+++ C#/Redis/NetFrameworkRedis/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost"))
+            {
+                var db = redis.GetDatabase();
+
+                var data = db.StringGet("batman");
+
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ C#/Redis/NetFrameworkRedis/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost"))
+            {
+                var db = redis.GetDatabase();
+
+                //db.StringSet("batman", "lalapo");
+
+                var data = db.StringGet("batman");
+
+                Console.WriteLine(data);
+            }
+
+
+
+        }
+    }
+}
