a40bfa7777c62415ae080e047371d18c601202e0
Gumbers <gustavomarinsuppi@gmail.com>
2020-05-27 20:17:43 -0300

    feat: adding serverless that can receive an image buffer, understand what is the image, translate to portuguese and return the set of portuguese text and its confidence

--- Javascript/Serverless/learning-serverless-aws/demo-3-image-analysis/image-analysis/handler.js
+++ Javascript/Serverless/learning-serverless-aws/demo-3-image-analysis/image-analysis/handler.js
@@ -1,18 +1,103 @@
-'use strict';
+'use strict'
+const { get } = require('axios')
 
-module.exports.main = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+class Handler {
+	constructor({ rekoSvc, translatorSvc }) {
+		this.rekoSvc = rekoSvc
+		this.translatorSvc = translatorSvc
+	}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+	async detectImageLabels(buffer) {
+		const result = await this.rekoSvc
+			.detectLabels({
+				Image: {
+					Bytes: buffer,
+				},
+			})
+			.promise()
+
+		const workingItems = result.Labels.filter(
+			({ Confidence }) => Confidence > 80
+		)
+
+		const names = workingItems.map(({ Name }) => Name).join(' and ')
+
+		return { names, workingItems }
+	}
+
+	async translateText(text) {
+		const params = {
+			SourceLanguageCode: 'en',
+			TargetLanguageCode: 'pt',
+			Text: text,
+		}
+
+		const { TranslatedText } = await this.translatorSvc
+			.translateText(params)
+			.promise()
+
+		return TranslatedText.split(' e ')
+	}
+
+	formatTextResults(texts, workingItems) {
+		const finalText = []
+
+		for (const indexText in texts) {
+			const nameInPortuguese = texts[indexText]
+			const confidence = workingItems[indexText].Confidence
+
+			finalText.push(`${confidence.toFixed(2)}% de ser um ${nameInPortuguese}`)
+		}
+		return finalText.join('\n')
+	}
+
+	async getImageBuffer(imageUrl) {
+		const response = await get(imageUrl, {
+			responseType: 'arraybuffer',
+		})
+
+		const buffer = Buffer.from(response.data, 'base64')
+
+		return buffer
+	}
+
+	async main(event) {
+		try {
+			const { imageUrl} = event.queryStringParameters
+
+			console.log('Downloading image')
+			const imgBuffer = await this.getImageBuffer(imageUrl)
+
+			console.log('Detecting labels')
+			const { names, workingItems } = await this.detectImageLabels(imgBuffer)
+
+			console.log('Translating labels')
+			const translatedTexts = await this.translateText(names)
+
+			console.log('Handling final message')
+			const finalText = this.formatTextResults(translatedTexts, workingItems)
+
+			console.log('finishing')
+
+			return {
+				statusCode: 200,
+				body: 'A imagem tem:\n'.concat(finalText),
+			}
+		} catch (error) {
+			console.log('Error', error.stack)
+			return {
+				statusCode: 500,
+				body: 'Internal server error!',
+			}
+		}
+	}
+}
+const aws = require('aws-sdk')
+const reko = new aws.Rekognition()
+const translator = new aws.Translate()
+const handler = new Handler({
+	rekoSvc: reko,
+	translatorSvc: translator,
+})
+
+module.exports.main = handler.main.bind(handler)
