bb7bf762f9a418132fda1f876d2cb76bd3a979c4
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-18 22:46:32 -0300

    chore: test data

--- Elixir/ElixirPhoenixApi/oceanize/lib/oceanize/repo.ex
+++ Elixir/ElixirPhoenixApi/oceanize/lib/oceanize/repo.ex
@@ -2,4 +2,14 @@ defmodule Oceanize.Repo do
   use Ecto.Repo,
     otp_app: :oceanize,
     adapter: Ecto.Adapters.Postgres
+
+    def init(_, config) do
+      config = config
+        |> Keyword.put(:username, "postgres")#System.get_env("PGUSER")
+        |> Keyword.put(:password, "postgres")
+        |> Keyword.put(:database, "oceanize")
+        |> Keyword.put(:hostname, "localhost")
+        |> Keyword.put(:port, "5432" |> String.to_integer)
+      {:ok, config}
+    end
 end
