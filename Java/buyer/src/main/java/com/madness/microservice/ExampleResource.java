2c2e84a835db1bc805a797937b96f858d4692c62
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-23 21:10:14 -0300

    chore: hello example

--- Java/buyer/src/main/java/com/madness/microservice/ExampleResource.java
+++ Java/buyer/src/main/java/com/madness/microservice/ExampleResource.java
@@ -1,10 +1,13 @@
 package com.madness.microservice;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 
 @Path("/hello")
@@ -12,8 +15,17 @@ public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-	@Timed
+    @Timed
     public Response hello() {
         return Response.ok("hello").build();
     }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Counted
+    public Response hello2(String message) {
+        return Response.ok(message).build();
+    }
+
+    
 }
