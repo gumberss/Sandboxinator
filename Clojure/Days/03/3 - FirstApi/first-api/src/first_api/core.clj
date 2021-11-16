f477172aac7c0c3235b83a125614ce7bd93e9c50
gumberss <gustavomarinsuppi@gmail.com>
2021-11-15 21:49:37 -0300

    chore: adding post method

--- Clojure/Days/03/3 - FirstApi/first-api/src/first_api/core.clj
+++ Clojure/Days/03/3 - FirstApi/first-api/src/first_api/core.clj
@@ -8,6 +8,26 @@
             [clojure.data.json :as json])
   (:gen-class))
 
+(def people-collection (atom []))
+
+(defn addperson [firstname surname]
+  (swap! people-collection conj {:firstname (str/capitalize firstname) :surname (str/capitalize surname)}))
+
+(defn getparameter [req pname] (get (:params req) pname))
+(defn addperson-handler [req]
+        {:status  200
+         :headers {"Content-Type" "text/json"}
+         :body    (-> (let [p (partial getparameter req)]
+                        (str (json/write-str (addperson (p :firstname) (p :surname))))))})
+
+(addperson "Functional" "Human")
+(addperson "Micky" "Mouse")
+
+(defn people-handler [req]
+        {:status  200
+         :headers {"Content-Type" "text/json"}
+         :body    (str (json/write-str @people-collection))})
+
   ; Simple Body Page
 (defn simple-body-page [req]
   {:status  200
@@ -33,6 +53,8 @@
   (GET "/" [] simple-body-page)
   (GET "/request" [] request-example)
   (GET "/hello" [] hello-name)
+  (GET "/people" [] people-handler)
+  (POST "/people/add" [] addperson-handler)
   (route/not-found "Error, page not found!"))
 
 
@@ -41,7 +63,7 @@
   [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     ; Run the server with Ring.defaults middleware
-    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    (server/run-server (wrap-defaults #'app-routes api-defaults) {:port port})
     ; Run the server without ring defaults
     ;(server/run-server #'app-routes {:port port})
     (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
