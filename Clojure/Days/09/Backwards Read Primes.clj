9c23cd80e0fe578f765a4a7ce571598d71512972
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2021-12-02 23:24:05 -0300

    Create Backwards Read Primes.clj

---
+++ Clojure/Days/09/Backwards Read Primes.clj
@@ -0,0 +1,31 @@
+(ns backwards-prime.core)
+
+(defn get-divisors [number]
+  (for [div (->> number (Math/sqrt) (int) (inc) (range 1))
+                 :let [r (mod number div)]
+                 :when (= r 0)] div))
+
+(defn prime? [number]
+  (cond 
+    (<= number 2) true
+    (-> number (get-divisors) (count) (<= 1)) true
+    :else false))
+  
+(defn reverse-number 
+  ([number reversed]
+    (cond
+      (= number 0) reversed
+      :else (recur (quot number 10) (+ (mod number 10) (* reversed 10)))))
+  ([number] (reverse-number number 0))
+)
+
+(defn is-reverse-prime [number]
+  (= true (prime? number) (prime?(reverse-number number))))
+
+(defn palindrome? [number] 
+  (= number (reverse-number number)))
+
+(defn backwards-prime [start stop]
+ (->> (range start (inc stop)) 
+   (filter is-reverse-prime)
+   (remove palindrome?)))
