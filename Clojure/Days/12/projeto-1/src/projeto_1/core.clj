2a50b699cf65171ae2df36b3c666bc4b49051e1e
gumberss <gustavomarinsuppi@gmail.com>
2021-12-06 22:10:57 -0300

    chore: adding first Clojure project generated by Leiningen

---
+++ Clojure/Days/12/projeto-1/src/projeto_1/core.clj
@@ -0,0 +1,13 @@
+(ns projeto-1.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(println (neg? (count [])))
+
+; Intelij
+; cmd shift + p - > executa a linha
+; cmd shift + k -> coloca a aexpressão dentro dos parenteses
+; cmd shift + j -> tira a aexpressão de dentro dos parenteses
