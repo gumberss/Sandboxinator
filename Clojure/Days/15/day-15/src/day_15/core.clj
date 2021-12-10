f2b57c9e50067b6c924efeaac37d697a46e47f61
gumberss <gustavomarinsuppi@gmail.com>
2021-12-10 20:39:24 -0300

    chore: defmulti

--- Clojure/Days/15/day-15/src/day_15/core.clj
+++ Clojure/Days/15/day-15/src/day_15/core.clj
@@ -3,14 +3,15 @@
 (defprotocol Discontable
   (get-discount [this]))
 
-(defrecord Food [ price perishable])
+(defrecord Food [price perishable])
 
 (extend-type Food Discontable
-  (get-discount [this] (let [price (get this :price)
-                             perishable (get this :perishable)]
-                         (if (= perishable true)
-                           (* price 0.1)
-                           0))))
+  (get-discount [this]
+    (let [price (get this :price)
+          perishable (get this :perishable)]
+      (if (= perishable true)
+        (* price 0.1)
+        0))))
 
 (defrecord Toy
   [price breakable]
@@ -29,4 +30,32 @@
 
 (pprint (get-discount (Toy. 100 true)))
 (pprint (get-discount (Toy. 100 false)))
+
+
+(defmulti testinator class)
+(defmethod testinator Food [food] (println "food" food))
+(defmethod testinator Toy [toy] (println "toy" toy))
+
+(testinator (->Food 100 true))
+
+(defn what [s]
+  (let [cost (:price s)]
+    (if (> cost 1000)
+      :expend-much
+      (class s))))
+
+(defmulti something what)
+(defmethod something Food [thing] (println "Something with food"))
+(defmethod something Toy [thing] (println "Something with toy"))
+(defmethod something :expend-much [thing] (println "Something that expend much"))
+(println "defmulti function")
+;(something "a")
+(something (Toy. 100 false))
+(something (Food. 100 false))
+(something (Food. 10000 false))
+
+
+
+
+
 
