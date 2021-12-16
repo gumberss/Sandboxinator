9a92a6e19513429e321da98b32062e0d95acc3f6
gumberss <gustavomarinsuppi@gmail.com>
2021-12-15 23:01:40 -0300

    chore: adding a java kafka producer

---
+++ Brokers/Kafka/Beginning/src/main/java/br/com/beginning/NewOrder.java
@@ -0,0 +1,35 @@
+package br.com.beginning;
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrder {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "1,hello,happy";
+        var record = new ProducerRecord("STORE_NEW_ORDER", value, value);
+        producer.send(record, (data, ex) -> {
+            if(ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println(data.topic() + " " + data.partition() + " " + data.offset());
+        }).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return properties;
+    }
+}
