3644ae2e80ec0dc449c071b50950270fa3e76f34
gumberss <gustavomarinsuppi@gmail.com>
2022-02-03 23:01:09 -0300

    chore: adding day 1

---
+++ F#/Day 01/FirstFSharpTest/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let printNames names  = 
+    
+    for name in names do
+        printfn $"Oi {name}"
+
+let names = 10
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    printNames ["Jarbas"; "Zorio"]
+    0 // return an integer exit code
