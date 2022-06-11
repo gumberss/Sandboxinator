75806e120ff218cd92002732fc207b833ef7429b
gumberss <gustavomarinsuppi@gmail.com>
2022-06-10 22:48:32 -0300

    feat: starting pedastal-components poc

---
+++ Clojure/POCs/pedestal-components/src/pedestal_components/core.clj
@@ -0,0 +1,6 @@
+(ns pedestal-components.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ Clojure/POCs/pedestal-components/test/pedestal_components/core_test.clj
@@ -0,0 +1,7 @@
+(ns pedestal-components.core-test
+  (:require [clojure.test :refer :all]
+            [pedestal-components.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
