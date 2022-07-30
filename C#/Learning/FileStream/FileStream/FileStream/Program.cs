09c68396c46e1779876f9e85a7c19e0e92f2fbe8
gumberss <gustavomarinsuppi@gmail.com>
2022-07-30 15:18:32 -0300

    feat: filestream

---
+++ C#/Learning/FileStream/FileStream/FileStream/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Text;
+try
+{
+
+    FileStream fs = new FileStream(
+     @"path",
+     FileMode.OpenOrCreate,
+     FileAccess.Write);
+
+    var alreadyInserted = 0;
+    for (int i = 0; i < 2000; i++)
+    {
+        for (int j = 0; j < 20000; j++)
+        {
+            var msg = Encoding.ASCII.GetBytes("Hello ");
+            fs.Write(msg, 0, msg.Length);
+        }
+        fs.Write(Encoding.ASCII.GetBytes(Environment.NewLine), 0, 1);
+    }
+    fs.Close();
+
+}
+catch (Exception ex)
+{
+
+}
