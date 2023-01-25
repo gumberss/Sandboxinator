77ff775f72e3597d2e83ff06ecf6a12dd8f2d988
gumberss <gustavomarinsuppi@gmail.com>
2023-01-25 14:40:49 -0300

    feat: testing C# records in a crazy way

---
+++ C#/RecordsTesting/RecordsTesting/logic/NameChanger.cs
@@ -0,0 +1,24 @@
+﻿using RecordsTesting.interfaces;
+using RecordsTesting.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordsTesting.logic
+{
+    public static class NameChanger
+    {
+        //this do not work with records so far
+        public static T ChangeName<T>(T data, String otherName) where T : struct, INamed
+        {
+            return data with { Name = otherName };
+        }
+
+        public static T ChangeNameRecord<T>(T data, String otherName) where T : NamedRecord
+        {
+            return data with { Name = otherName };
+        }
+    }
+}
---
+++ C#/RecordsTesting/RecordsTesting/models/Person.cs
@@ -0,0 +1,15 @@
+﻿using RecordsTesting.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordsTesting.models
+{
+    public record Person : NamedRecord, INamed
+    {
+        public int Age { get; set; }
+        public String Cpf { get; set; }
+    }
+}
